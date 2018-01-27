70f0ee55771502e4f485da555471f2f2efb814bd
Eduardo Pastre <dupastre@gmail.com>
2018-01-27 11:25:28 -0200

    refactoring test feature home_spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Testing the home page", :type => :feature do
   it "loading home page" do
-    visit "/"
+    visit root_path
 
     expect(page.status_code).to be(200)
   end
